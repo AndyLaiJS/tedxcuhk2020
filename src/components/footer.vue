6df919fa9c031774ac44cc2fc2f2bccc49649c90
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-12-26 11:59:08 +0800

    footer: Subscribe button, and changes to layout

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="container">
         <div class="content">
-            <img src="../assets/TEDxCUHKemblem.png" height="100px">
+            <div class="footercontent">
+                <img src="../assets/TEDxCUHKemblem.png" height="100px"><br>
+                This independent TEDx event is operated under license from <a href="https://www.ted.com" target="_blank" rel="noreferrer">TED</a>.
+            </div>
+
+            <div class="subscribe-wrapper">
+                <button id="button">
+                    SUBSCRIBE TO OUR NEWSLETTER
+                </button>
+            </div>
 
             <div class="socialmedia">
 
@@ -13,17 +22,16 @@
                 <a target="_blank" href="https://www.youtube.com/user/TEDxTalks" rel="noreferrer"><img alt="Instagram icon" src="../assets/YouTubeIcon.png" style="height: 25px; padding-bottom: 2px;"></a> 
 
                 <br>
-                <span style="padding: 10px 10px 0 10px;"><router-link to="PrivPolicy">Privacy Policy</router-link></span> | <span style="padding: 10px 0 0 10px;"><a target="_blank" href="mailto:tedxcuhkteam@gmail.com">Mail</a></span> <!-- Don't judge me again -->
+                <span style="padding: 10px 10px 0 10px;"><router-link to="PrivPolicy">Privacy Policy</router-link></span> | 
+                <span style="padding: 10px 0 0 10px;"><a target="_blank" href="mailto:tedxcuhkteam@gmail.com">Mail</a></span> <!-- Don't judge me again -->
             </div>
             
         </div>
-        <div class="footercontent"> 
-            This independent TEDx event is operated <br>under license from <a href="https://www.ted.com" target="_blank" rel="noreferrer">TED</a>.
-        </div>
+        
         <br>
         <hr style="border-color: gray;">
 
-        <div class="content" style="display: block; padding: 20px 0 25px 0; font-size: 13px; text-align: center;">
+        <div class="copyright"> 
             Copyright © TEDxCUHK 2020. All rights reserved.
         </div>
     </div>
@@ -36,6 +44,27 @@ export default {
 </script>
 
 <style scoped>
+#button:hover {
+    color: #e62b1e;
+    background-color: transparent;
+    transition: ease-in-out 0.17s;
+}
+#button {
+    transition: ease-in-out 0.17s;
+    color: white;
+    border: None;
+    position: relative;
+    top: 26%;
+    background-color: #e62b1e;
+    border: 5px solid #e62b1e;
+    padding: 17px;
+    font-weight: bolder;
+    font-size: 13px;
+    cursor: pointer;
+}
+.subscribe-wrapper {
+    text-align: center;
+}
 #hash {
     font-weight: bolder;
     font-size: 35px;
@@ -47,9 +76,15 @@ export default {
 }
 .content {
     display: grid;
-    grid-template-columns: 50% 50%;
+    grid-template-columns: 33% 34% 33%;
     padding-top: 30px;
     margin: 0 17% 0 17%;
+}
+.copyright {
+    display: block; 
+    padding: 20px 0 25px 0; 
+    font-size: 13px; 
+    text-align: center;
 }
 .footercontent {
     font-size: 13px;
